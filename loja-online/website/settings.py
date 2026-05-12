70f8e9d8bb4dec5f6d766a6cd8119c2a0f52af51
Rui Costa <rui.costa@meu.ipta.pt>
2026-05-12 11:47:24 +0100

    setup database with mysql

--- loja-online/website/settings.py
+++ loja-online/website/settings.py
@@ -17,7 +17,7 @@ import os
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-environ.Env.read_env(os.path.join(BASE_DIR, '.env'))
+environ.Env.read_env(os.path.join(BASE_DIR, '../.env'))
 
 env = environ.Env(
     DEBUG=(bool, False),
@@ -85,8 +85,10 @@ WSGI_APPLICATION = 'website.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        "OPTIONS": {
+            "read_default_file": "../../db.conf",
+        },
     }
 }
 
